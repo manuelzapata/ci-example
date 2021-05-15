5b60ec2680af3e1dfd50cb0a74b0628dbc795afd
Manuel Zapata <manuel@makrwatch.com>
2021-05-15 09:20:39 -0500

    Improved factory code readability.

--- src/main/java/restriccion/FabricaServicio.java
+++ src/main/java/restriccion/FabricaServicio.java
@@ -5,20 +5,11 @@ import java.math.BigDecimal;
 public class FabricaServicio {
 
     public static ServicioRestriccion getServicio(String ciudad) {
-        ServicioRestriccion resultado = null;
-
-        switch (ciudad) {
-            case "Medellín":
-                resultado = new ServicioRestriccionMedellin();
-                break;
-            case "Cali":
-                resultado = new ServicioRestriccionCali();
-                break;
-            case "Bogotá":
-                resultado = new ServicioRestriccionBogota();
-                break;
-            default:
-                throw new UnsupportedOperationException("La ciudad " + ciudad + " no es soportada.");
+        ServicioRestriccion resultado = switch (ciudad) {
+            case "Medellín" -> new ServicioRestriccionMedellin();
+            case "Cali" -> new ServicioRestriccionCali();
+            case "Bogotá" -> new ServicioRestriccionBogota();
+            default -> throw new UnsupportedOperationException("La ciudad " + ciudad + " no es soportada.");
         };
         return resultado;
     }
